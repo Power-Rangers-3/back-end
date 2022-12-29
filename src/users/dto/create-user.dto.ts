12a511125fa5d0b5f4d5dcbaf6a82908049581a8
primky <fakeprimky@gmail.com>
2022-12-29 10:45:30 +0300

    init: created docker composition

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
 
   @ApiProperty({example: 'myemail@gmail.com', description: 'uniq users email'})
+  @IsString({message: 'should be string'})
+  @IsEmail({},{message: 'uncorrect email'})
   readonly email: string;
   @ApiProperty({example: 'password123', description: 'non uniq password'})
+  @IsString({message: 'should be string'})
+  @Length(4,16, {message: 'uncorrect length (more than 4, less than 16)'})
   readonly password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards, UsePipes } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from "./users.service";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
@@ -6,6 +6,7 @@ import { User } from "./user.models";
 import { Roles } from "src/auth/roles-auth.decorator";
 import { RolesGuard } from "src/auth/roles.guard";
 import { AddRoleDto } from "./dto/add-role.dto";
+import { ValidationPipe } from "../pipes/validation.pipe";
 
 @ApiTags('Users')
 @Controller('users')
@@ -15,6 +16,7 @@ export class UsersController {
 
   @ApiOperation({summary: 'creating user'})
   @ApiResponse({status: 200, type: User})
+  @UsePipes(ValidationPipe)
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.userService.createUser(userDto);
