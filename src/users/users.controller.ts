37d2a72beaa114cd3c4c28b31cf412d39af31dd0
DESKTOP-G3EI2ED\Павел <pavel.chernenko97@gmail.com>
2023-01-29 14:49:43 +0300

    feat: add update and delete field for user data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,40 +1,65 @@
-import { Body, Controller, Get, Post, UseGuards, UsePipes } from "@nestjs/common";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { UsersService } from "./users.service";
-import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
-import { User } from "./user.models";
-import { Roles } from "src/auth/roles-auth.decorator";
-import { RolesGuard } from "src/auth/roles.guard";
-import { AddRoleDto } from "./dto/add-role.dto";
-import { ValidationPipe } from "../pipes/validation.pipe";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from './user.models';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
+import { ValidationPipe } from '../pipes/validation.pipe';
+import { JwtAuthGuard } from './../auth/jwt-auth.guard';
 
 @ApiTags('Users')
 @Controller('users')
 export class UsersController {
-
   constructor(private userService: UsersService) {}
 
-  @ApiOperation({summary: 'creating user'})
-  @ApiResponse({status: 200, type: User})
+  @ApiOperation({ summary: 'creating user' })
+  @ApiResponse({ status: 200, type: User })
   @UsePipes(ValidationPipe)
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.userService.createUser(userDto);
   }
 
-  @ApiOperation({summary: 'getting all users'})
-  @ApiResponse({status: 200, type: [User]})
+  @ApiOperation({ summary: 'getting all users' })
+  @ApiResponse({ status: 200, type: [User] })
   @Get()
   getAll() {
     return this.userService.getALlUsers();
   }
 
-  @ApiOperation({summary: 'add one more role for user / only for admin'})
-  @ApiResponse({status: 200})
-  @Roles("ADMIN")
+  @ApiOperation({ summary: 'add one more role for user / only for admin' })
+  @ApiResponse({ status: 200 })
+  @Roles('ADMIN')
   @UseGuards(RolesGuard)
   @Post('/role')
   addRole(@Body() dto: AddRoleDto) {
     return this.userService.addRole(dto);
+  }
+
+  @ApiOperation({ summary: 'update user data' })
+  @ApiResponse({ status: 200, type: User })
+  @UseGuards(JwtAuthGuard)
+  @Patch('/:id/update')
+  update(@Param('id') id: string, @Body() dto: Partial<User>) {
+    return this.userService.updateUser(id, dto);
+  }
+  @ApiOperation({ summary: 'delete user field data' })
+  @ApiResponse({ status: 200, type: User })
+  @UseGuards(JwtAuthGuard)
+  @Delete('/:id/delete')
+  delete(@Param('id') id: string, @Body() field: string) {
+    return this.userService.deleteUserField(id, field);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,42 +1,69 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { User } from "./user.models";
-import { InjectModel } from "@nestjs/sequelize";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { RolesService } from "../roles/roles.service";
-import { AddRoleDto } from "./dto/add-role.dto";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { User } from './user.models';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDto } from './dto/create-user.dto';
+import { RolesService } from '../roles/roles.service';
+import { AddRoleDto } from './dto/add-role.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-
-  constructor(@InjectModel(User) private userRepository: typeof User,
-              private roleService: RolesService) {}
+  constructor(
+    @InjectModel(User) private userRepository: typeof User,
+    private roleService: RolesService,
+  ) {}
 
   async createUser(dto: CreateUserDto) {
-    const user = await this.userRepository.create(dto)
-    const role = await this.roleService.getRoleByValue("USER")
-    await user.$set('roles', [role.id])
-    user.roles = [role]
+    const user = await this.userRepository.create(dto);
+    const role = await this.roleService.getRoleByValue('USER');
+    await user.$set('roles', [role.id]);
+    user.roles = [role];
     return user;
   }
 
   async getALlUsers() {
-    const users = await this.userRepository.findAll({include: {all: true}});
+    const users = await this.userRepository.findAll({ include: { all: true } });
     return users;
   }
 
   async getUserByEmail(email: string) {
-    const user = await this.userRepository.findOne({where: {email}, include: {all: true}})
+    const user = await this.userRepository.findOne({
+      where: { email },
+      include: { all: true },
+    });
     return user;
   }
 
   async addRole(dto: AddRoleDto) {
-    const user = await this.userRepository.findByPk(dto.userId)
-    const role = await this.roleService.getRoleByValue(dto.value)
+    const user = await this.userRepository.findByPk(dto.userId);
+    const role = await this.roleService.getRoleByValue(dto.value);
     if (role && user) {
-      await user.$add('role', role.id)
+      await user.$add('role', role.id);
       return dto;
     }
-    throw new HttpException('User or role do not found', HttpStatus.NOT_FOUND)
+    throw new HttpException('User or role do not found', HttpStatus.NOT_FOUND);
   }
 
+  async updateUser(id: string, userDto: Partial<User>) {
+    const user = await this.userRepository.findByPk(+id);
+    if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    if (!userDto) throw new HttpException('No content', HttpStatus.NO_CONTENT);
+    if (userDto.password)
+      userDto.password = await bcrypt.hash(userDto.password, 5);
+    await user.update(userDto);
+    return user;
+  }
+
+  async deleteUserField(id: string, userField: string) {
+    const user = await this.userRepository.findByPk(+id);
+    if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    if (userField.includes('password') || userField.includes('email'))
+      throw new HttpException(
+        'password or email not be deleted',
+        HttpStatus.BAD_REQUEST,
+      );
+    user[userField] = null;
+    await user.save();
+    return user;
+  }
 }
